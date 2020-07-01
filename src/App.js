16093dbea1cd009f5a0f0bb49e65be389b463386
Joe <weonmart12@gmail.com>
2020-06-30 21:20:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count +1
+    });
+  };
+
+  render() {
+    
+    return (
+      <div>
+        <h2>Counter app</h2>
+        <button onClick={this.increment}>
+          Clicked {this.state.count} times
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
